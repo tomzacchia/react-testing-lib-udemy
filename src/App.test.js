f726ec712002a43ff46a18e07ee26447964d478a
tomzacchia <tomzacchia@live.com>
2022-03-19 15:28:00 -0400

    Remove comments

--- src/App.test.js
+++ src/App.test.js
@@ -18,30 +18,22 @@ test("button has correct initial color", () => {
 test("initial condition", () => {
   render(<App />);
 
-  // check that the button starts out enabled
   const button = screen.getByRole("button", { name: /change to blue/i });
   expect(button).toBeEnabled();
 
-  // check that the checkbox starts out unchecked
   const checkbox = screen.getByRole("checkbox");
   expect(checkbox).not.toBeChecked();
 });
 
-/**
- * Spec: 1) When checkbox is unchecked the button is enabled
- *       2) when checkbox is checked the button is disabled
- */
 test("checkbox disables on first click and enables on second click", () => {
   render(<App />);
 
   const button = screen.getByRole("button", { name: /change to blue/i });
   const checkbox = screen.getByRole("checkbox");
 
-  // first click, checkbox is checked
   fireEvent.click(checkbox);
   expect(button).toBeDisabled();
 
-  // first click, checkbox is unchecked
   fireEvent.click(checkbox);
   expect(button).toBeEnabled();
 });
